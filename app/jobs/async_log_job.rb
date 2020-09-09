e29451cf4b7443dc993e480bd20435177c104f2b
ryosuke takasawa <ten.r0619@gmail.com>
2020-09-10 05:59:04 +0900

    225p

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -1,7 +1,20 @@
 class AsyncLogJob < ApplicationJob
-  queue_as :default
+  queue_as do
+    case self.aruguments.first[:message]
+      when "to async_log"
+        :async_log
+      else
+        :default
+    end
+  end
 
   def perform(message: "hello")
     AsyncLog.create!(message: message)
+  end
+
+  retry_on StandardError, wait: 5.seconds, attempts: 3
+
+  discard_on StandardError do |job, error|
+    SomeNotifier.push(error)
   end
 end
